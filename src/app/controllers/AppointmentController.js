878a2ebea90f21facb88ca161608038d447ed90b
Laercio Nascimento <laercio@lavimco.com>
2020-02-24 03:52:00 -0300

    configure email template

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -128,6 +128,11 @@ class AppointmentControler {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -152,7 +157,14 @@ class AppointmentControler {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
