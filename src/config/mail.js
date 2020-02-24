f792811bc47633e533c0f8c82eb16b63956accc8
Laercio Nascimento <laercio@lavimco.com>
2020-02-23 21:46:22 -0300

    set nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8e3ea70122955e',
+    pass: '33378bf56f3f1d',
+  },
+  default: {
+    from: 'Equipe Barbeber <noreply@barbeber.com>',
+  },
+};
